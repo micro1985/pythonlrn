3e11b561d305523d34b6c387482ce42cd9cb4b86
DV <DV@itstep.lan>
2021-03-18 19:44:24 +0300

    lesson3

---
+++ lesson3/lesson3_1.py
@@ -0,0 +1,35 @@
+def generateList(quant, minVal=0, maxVal=100):
+    
+    from random import randint
+    lst = []
+    for i in range(quant):
+        lst.append(randint(minVal,maxVal))
+
+    print(lst)
+    return lst
+
+def funcMinMaxAliquot(lis,n=1):
+
+    assert type(lis) == list
+    
+    mn = min(lis)
+    mx = max(lis)
+
+    print("Min: ", mn)
+    print("Max: ", mx)
+
+    aliquuotlst = []
+    for i in lis:
+        if i % n == 0:
+            aliquuotlst.append(i)
+
+    if aliquuotlst:
+        return aliquuotlst
+    else:
+        print("No aliquot values!")
+
+val = generateList(20)
+alinum = int(input("Enter number for aliquot: "))
+val2 = funcMinMaxAliquot(val, alinum)
+
+print("Aliquot numbers: ", val2)
